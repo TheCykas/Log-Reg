f2b75e6b770bbafc1fadddb48c4e669ca6da1739
Cykablyadd <62244863+Cykablyadd@users.noreply.github.com>
2020-03-16 16:29:10 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ public class Main {
         System.out.println("Test");
         for (int i = 0; i < 5; i++) {
             System.out.println(i);
+            System.out.println("omk 3ndna");
+            
         }
     }
     
